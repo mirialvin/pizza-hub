73e2512571ab2441795fc6ef0214f3c5412b3da7
mirialvin <alvinhiuhu@gmail.com>
2021-09-19 23:24:04 +0300

    add images

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// Javascript
+var customerMessage = [];
+
+function showCustomerMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("textarea").value;
+
+    if (name && email && message) {
+        customerMessage.push(name, email, message);
+        return name;
+    }
+}
 
 // jQuery
 $(document).ready(function() {
@@ -23,7 +36,35 @@ $(document).ready(function() {
         }
         hideAlert();
     });
+    $("#button").click(function() {
+        if (showCustomerMessage()) {
+            $("#message-alert").text("Hi " + showCustomerMessage() + ", \nThank you for reaching out to us, we will respond as soon as possible.");
+            $("#message-alert").addClass("alert-success");
+            $("#message-alert").removeClass("alert-danger");
+            $("#message-alert").show();
+        } else {
+            $("#message-alert").text("Please fill in all fields");
+            $("#message-alert").removeClass("alert-success");
+            $("#message-alert").addClass("alert-danger");
+            $("#message-alert").show();
+        }
+
+
+    });
+
+    $(button).on("click", function() {
+        alertFadeOutTime = 5000;
+
+        function hideAlert() {
+            var messageAlert = $('#message-alert');
+            messageAlert.fadeOut(alertFadeOutTime);
+        }
+        hideAlert();
+    });
 });
+
+
+
 
 
 
