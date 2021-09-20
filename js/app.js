fc17434ccd72aa789f5fb741dee1c24975ac9b17
mirialvin <alvinhiuhu@gmail.com>
2021-09-20 09:18:33 +0300

    add paragraph

--- js/app.js
+++ js/app.js
@@ -36,35 +36,7 @@ $(document).ready(function() {
         }
         hideAlert();
     });
-    $("#button").click(function() {
-        if (showCustomerMessage()) {
-            $("#message-alert").text("Hi " + showCustomerMessage() + ", \nThank you for reaching out to us, we will respond as soon as possible.");
-            $("#message-alert").addClass("alert-success");
-            $("#message-alert").removeClass("alert-danger");
-            $("#message-alert").show();
-        } else {
-            $("#message-alert").text("Please fill in all fields");
-            $("#message-alert").removeClass("alert-success");
-            $("#message-alert").addClass("alert-danger");
-            $("#message-alert").show();
-        }
-
-
-    });
-
-    $(button).on("click", function() {
-        alertFadeOutTime = 5000;
-
-        function hideAlert() {
-            var messageAlert = $('#message-alert');
-            messageAlert.fadeOut(alertFadeOutTime);
-        }
-        hideAlert();
-    });
 });
-
-
-
 
 
 
